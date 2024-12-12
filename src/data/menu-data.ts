ded3bb89dcb24a0763407888b4cacda9337647c2
renatorrocha <renatorrodrigues2002@gmail.com>
2024-12-12 17:04:05 -0300

    chore:update menu data

--- src/data/menu-data.ts
+++ src/data/menu-data.ts
@@ -36,7 +36,7 @@ export const menuData = [
         name: "Kafita bovina",
         description: "Espetinho especial de kafita bovina",
         price: 12.0,
-        image: "https://via.placeholder.com/150",
+        image: "./kafta-bovina.jpg",
       },
       {
         name: "Kafita de frango",
@@ -97,6 +97,43 @@ export const menuData = [
         description: "Espetinho crocante do meio da asa",
         price: 12.0,
         image: "/meio-da-asa.jpg",
+      },
+    ],
+  },
+  {
+    group: "Refeições",
+    items: [
+      {
+        name: "Jantinha Completa",
+        description:
+          "Uma refeição composta por arroz, feijão tropeiro, mandioca, vinagrete e um espetinho de sua escolha.",
+        price: 25.0,
+        image:
+          "https://th.bing.com/th/id/OIP.LuHMiWMQcbkNcytPqI8eWAHaE8?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Feijão Tropeiro",
+        description:
+          "Uma generosa porção do nosso tradicional feijão tropeiro, feito com ingredientes frescos e um toque especial que traz todo o sabor da culinária mineira.",
+        price: 8.0,
+        image:
+          "https://receitarapido.com/wp-content/uploads/2022/05/Feijao-Tropeiro.jpg",
+      },
+      {
+        name: "Arroz",
+        description:
+          "Porção de arroz soltinho, fresquinho e feito com todo o cuidado, o acompanhamento perfeito para qualquer refeição.",
+        price: 5.0,
+        image:
+          "https://th.bing.com/th/id/OIP.1CW0IiOcrAh88XpTVeIcygHaDS?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "Mandioca",
+        description:
+          "Porção de mandioca crocante por fora e macia por dentro, temperada na medida certa para proporcionar um sabor incrível.",
+        price: 3.0,
+        image:
+          "https://th.bing.com/th/id/OIP.HdCyTIwxuEa0rQnJuf9THwHaDk?rs=1&pid=ImgDetMain",
       },
     ],
   },
